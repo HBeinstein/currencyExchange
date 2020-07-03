28f748911f9914e3885e11496ff9c7a3c1322a28
Hannah Beinstein <hannah.beinstein@gmail.com>
2020-07-03 16:30:02 -0700

    fix bug in main.js

--- src/main.js
+++ src/main.js
@@ -11,22 +11,25 @@ let currencies = getCurrencyAmounts();
     $('.money-type-amount').empty();
 
     let usdAmount = parseFloat($('.usd-input').val())
+
     let currencyAbbreviation = $('#new-currency-select').val();
 
     Promise.resolve(currencies) 
     .then(function(resolvedCurrencies){
     console.log(resolvedCurrencies[currencyAbbreviation])
 
-      if(resolvedCurrencies[currencyAbbreviation] === undefined){
+      if(!resolvedCurrencies[currencyAbbreviation]){
         throw Error("sorry, information for this currency type isn't available")
       } 
       const newCalcAmount = usdAmount / resolvedCurrencies[currencyAbbreviation];
       console.log(newCalcAmount)
+
+      $('.money-type-amount').append(`You have ${newCalcAmount} ${currencyAbbreviation}`);
     })
       .catch(function(error) {
         return error
       });
-
-      $('.money-type-amount').append(`You have ${newCalcAmount} ${currencyAbbreviation}`);
+      
+      
     })
   });
