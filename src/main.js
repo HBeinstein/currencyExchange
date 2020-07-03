f4d0e4d64325e6523923f7afbfc4a4dc59fdf214
Hannah Beinstein <hannah.beinstein@gmail.com>
2020-07-03 14:07:46 -0700

    add newcalcAmount var to main.js to calc new currency amount

--- src/main.js
+++ src/main.js
@@ -8,12 +8,15 @@ let currencies = getCurrencyAmounts();
   $('.user-input-form').submit(function() {
     event.preventDefault()
 
-    let usdAmount = $('.usd-input')
+    let usdAmount = parseFloat($('.usd-input').val())
     let currencyAbbreviation = $('#new-currency-select').val();
 
     Promise.resolve(currencies) 
     .then(function(resolvedCurrencies){
-      $('.money-type-amount').append(`${resolvedCurrencies[currencyAbbreviation]} ${currencyAbbreviation}`)
+      const newCalcAmount = usdAmount / resolvedCurrencies[currencyAbbreviation];
+      console.log(newCalcAmount)
+
+      $('.money-type-amount').append(`You have ${newCalcAmount} ${currencyAbbreviation}`);
     })
     
     
