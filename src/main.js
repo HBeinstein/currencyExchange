68e6af8e424d07840e6f244b2d90e6be6bbd40f0
Hannah Beinstein <hannah.beinstein@gmail.com>
2020-07-03 14:03:06 -0700

    add variable to number input in form

--- src/main.js
+++ src/main.js
@@ -2,11 +2,20 @@ import $ from 'jquery'
 import './styles.css';
 import {getCurrencyAmounts} from './services/currencies.js';
 
-$( document ).ready(function() {
-  let currencies = getCurrencyAmounts();
-  
-  Promise.resolve(currencies) 
-  .then(function(resolvedCurrencies){
-    console.log(resolvedCurrencies)
-  })
+$(document).ready(function() {
+let currencies = getCurrencyAmounts();
+
+  $('.user-input-form').submit(function() {
+    event.preventDefault()
+
+    let usdAmount = $('.usd-input')
+    let currencyAbbreviation = $('#new-currency-select').val();
+
+    Promise.resolve(currencies) 
+    .then(function(resolvedCurrencies){
+      $('.money-type-amount').append(`${resolvedCurrencies[currencyAbbreviation]} ${currencyAbbreviation}`)
+    })
+    
+    
+  });
 });
